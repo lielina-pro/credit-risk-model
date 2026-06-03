ab73a3d1953b3d2ac8ac7f4899be42d883cd9620
lielina fekadu <lielinafekadu85@gmail.com>
2026-06-03 21:21:05 +0300

    Comment out 'os' import in train.py
    Comment out the import statement for 'os' in train.py.

--- src/train.py
+++ src/train.py
@@ -17,7 +17,7 @@ MLflow UI:
 
 import argparse
 import logging
-import os
+# import os
 import warnings
 from pathlib import Path
 
@@ -463,4 +463,3 @@ if __name__ == "__main__":
     )
     args = parser.parse_args()
     main(data_path=args.data, quick=args.quick)
-
